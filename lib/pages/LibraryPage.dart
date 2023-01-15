df9a7a1c10c8e79000c2f4f960332a343406f053
minseok128 <minseok128128@gmail.com>
2023-01-15 22:00:08 +0900

    Add Room Card

--- lib/pages/LibraryPage.dart
+++ lib/pages/LibraryPage.dart
@@ -1,4 +1,5 @@
 //장민석님 page
+
 import "package:flutter/material.dart";
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -33,6 +34,7 @@ class _LibraryPageState extends State<LibraryPage> {
     setState(() {
       selectedId = id;
       prefs.setInt('libraryPage_selectedId', id);
+      print('Selected ID: $selectedId');
     });
   }
 
@@ -40,20 +42,18 @@ class _LibraryPageState extends State<LibraryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 127,
         elevation: 0,
         backgroundColor: Colors.white.withOpacity(0),
-        title: const Text(
-          "열람실 좌석 현황",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 26,
-          ),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+        flexibleSpace: Column(
           children: [
+            const Text(
+              "열람실 좌석 현황",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+              ),
+            ),
             Container(
               margin: const EdgeInsets.symmetric(
                 vertical: 20,
@@ -87,8 +87,111 @@ class _LibraryPageState extends State<LibraryPage> {
               ),
             ),
             Container(
-              height: 0.5,
+              height: 1,
               color: Colors.grey,
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: const [
+                          Icon(Icons.star_rounded),
+                        ],
+                      ),
+                    ),
+                    const RoomCard(),
+                    const RoomCard(),
+                    const RoomCard(),
+                    Container(
+                      height: 0.5,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const RoomCard(),
+                    const RoomCard(),
+                    const RoomCard(),
+                    const RoomCard(),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RoomCard extends StatelessWidget {
+  const RoomCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      margin: const EdgeInsets.only(bottom: 15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: const Color(0xFFECF0FF),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(18),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.grey,
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  '제 1 열람실',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  '총좌석: 320 | 사용중: 120 | 빈좌석: 200',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text('이용률'),
+                SizedBox(
+                  height: 5,
+                ),
+                Text('70%'),
+              ],
             )
           ],
         ),
@@ -97,7 +200,7 @@ class _LibraryPageState extends State<LibraryPage> {
   }
 }
 
-class LocalButton extends StatefulWidget {
+class LocalButton extends StatelessWidget {
   final String name;
   final IconData iconData;
   final bool isSelected;
@@ -113,14 +216,9 @@ class LocalButton extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<LocalButton> createState() => _LocalButtonState();
-}
-
-class _LocalButtonState extends State<LocalButton> {
-  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => widget.onTapLocationButton(widget.myId),
+      onTap: () => onTapLocationButton(myId),
       child: Container(
         width: 100,
         decoration: BoxDecoration(
@@ -129,17 +227,18 @@ class _LocalButtonState extends State<LocalButton> {
         child: Column(
           children: [
             Icon(
-              widget.iconData,
-              color: widget.isSelected ? Colors.blue : Colors.black,
+              iconData,
+              color: isSelected ? Colors.blue : Colors.black,
             ),
             const SizedBox(
               height: 10,
             ),
             Text(
-              widget.name,
+              name,
               style: TextStyle(
                 fontSize: 16,
-                color: widget.isSelected ? Colors.blue : Colors.black,
+                fontWeight: isSelected ? FontWeight.w600 : FontWeight.w500,
+                color: isSelected ? Colors.blue : Colors.black,
               ),
             ),
           ],
